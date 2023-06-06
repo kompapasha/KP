756840cb265e3ad95e20ea57e2c778268f1213fb
kompapasha <116148890+kompapasha@users.noreply.github.com>
2023-06-06 11:56:47 +0300

    Add files via upload

---
+++ Лаба 2/KPLabWork2/Controls/BorderedEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+namespace KPLabWork2.Controls
+{
+    public class BorderedEntry : Entry
+    {
+
+    }
+}
+
---
+++ Лаба 2/KPLabWork2X.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using KPLabWork2.ViewModels;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.Maui.Devices;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace KPLabWork2X.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var viewModel = new MainViewModel();
+            Assert.Equal(1, viewModel.CalculateOne(5));
+        }
+    }
+}
